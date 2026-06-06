4dfdb76814ee60ad3d8374a928acb1573d55d840
meti2727 <metiseboka2025@gmail.com>
2026-06-06 10:31:03 +0300

    sign uplogic

--- Backend/app/routers/auth.py
+++ Backend/app/routers/auth.py
@@ -20,4 +20,25 @@ def signup(user:UserCreate, db: Session= Depends(get_db)):
      User.email == user.email 
     ).first() 
 
+    if existing_user:
+         raise HTTPException(
+              status_code = 400,
+              deatil= "Email aready registerd"
+         )
 
+    new_user = User (
+       username = user.username,
+       email= user.email,
+       hashed_password= hash_password(user.password)
+
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+  
+
+    return {
+         "message":"User created successfully",
+         "user_id": new_user.id
+    }
--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
+
 from app.db.base import Base
 from app.db.database import engine
+
 from app.models.user import User
+from app.routers.auth import router as auth_router
+
+
+
 
 app =FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 
-@app.get("/")
-def home():
-    return {"message": "Hello World"}
+app.include_router(auth_router)
+
+
